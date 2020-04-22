63a52e8c104cea6e3fa8f44b5e81e74524f6c8c2
Alex Pariseau <arpariseau@gmail.com>
2020-04-22 11:00:20 -0600

    Can get summary of recipe

--- lib/cook_book.rb
+++ lib/cook_book.rb
@@ -1,8 +1,11 @@
+require 'date'
+
 class CookBook
-attr_reader :recipes
+attr_reader :recipes, :date
 
     def initialize
       @recipes = []
+      @date = Date.today.strftime("%m-%d-%Y")
     end
 
     def add_recipe(recipe)
@@ -17,6 +20,21 @@ attr_reader :recipes
 
     def highest_calorie_meal
       @recipes.max_by {|recipe| recipe.total_calories}
+    end
+
+    def summary
+      @recipes.map do |recipe|
+        {name: recipe.name,
+         details: {
+           ingredients: recipe.ingredients_required.sort_by do |ingredient, amount|
+             ingredient.calories * amount
+           end.reverse.map do |ingredient, amount|
+             {ingredient: ingredient.name,
+             amount: "#{amount} #{ingredient.unit}"}
+           end,
+           total_calories: recipe.total_calories}
+         }
+      end
     end
 
 end
